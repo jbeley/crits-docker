cd7dd43f164b0b41dee5ac7d2eae14ed8b9c9e5f
Jeff Beley <jbeley@gmail.com>
2017-11-08 19:11:34 +0000

    added defaults to allow for non-compose build

--- firstboot.sh
+++ firstboot.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+CRITS_HOME=${CRITS_HOME:-/data/crits}
+CRITS_SERVICES_USED=${CRITS_SERVICES:-/data/crits_services}
+CRITS_SERVICES=${CRITS_SERVICES:-/data/crits_services.unused}
+MONGO_HOST=${MONGO_HOST:-localhsot}
+CRITS_FQDN=${CRITS_HOME:-localhost}
+CRITS_ADMINEMAIL=${CRITS_ADMINEMAIL:-crits@example.com}
+CRITS_ADMINFN=${CRITS_ADMINFN:-Thomas}
+CRITS_ADMINLN=${CRITS_ADMINLN:-Anderson}
+CRITS_ADMINPW=${CRITS_ADMINPW:-$(openssl rand -base64 32)}
+CRITS_ADMIN=${CRITS_ADMIN:-critsadmin}
+CRITS_ORG=${CRITS_ORG:-DFIR}
+SPLUNK_URL=${SPLUNK_URL:-https://splunk:8000/}
+
+
 set -e -u
 sleep 3
 if [ -f /firstboot.tmp ] ;
@@ -17,7 +32,7 @@ then
 		mkdir -p /etc/apache2/conf.d/ && \
         a2enmod ssl && \
         SK=$(python $CRITS_HOME/contrib/gen_secret_key.py) && \
-        sed -i -e "s/^MONGO_HOST = 'localhost'/MONGO_HOST = 'mongo'/g" $CRITS_HOME/crits/config/database.py && \
+        sed -i -e "s/^MONGO_HOST = 'localhost'/MONGO_HOST = \'$MONGO_HOST\'/g" $CRITS_HOME/crits/config/database.py && \
         sed -i -e "s/^\(SECRET_KEY = \).*$/\1\'${SK}\'/1" $CRITS_HOME/crits/config/database.py
 
         python $CRITS_HOME/manage.py create_default_collections && \
@@ -31,10 +46,12 @@ then
         python $CRITS_HOME/manage.py setconfig instance_url $CRITS_FQDN \
         python $CRITS_HOME/manage.py setconfig service_dirs $CRITS_SERVICES_USED \
         python $CRITS_HOME/manage.py setconfig splunk_search_url \
-        'https://splunk.beley.local:8000/en-US/app/search/search?earliest=-24h%40h&latest=now&display.page.search.mode=verbose&dispatch.sample_ratio=1&q=search%20*%20%20index%3Dbro%20' \
+        "$SPLUNK_URL/en-US/app/search/search?earliest=-24h%40h&latest=now&display.page.search.mode=verbose&dispatch.sample_ratio=1&q=search%20*%20%20index%3Dbro%20" \
 		python $CRITS_HOME/manage.py users -A  -u $CRITS_ADMIN
         chown -R www-data $CRITS_HOME $CRITS_SERVICES
         rm /firstboot.tmp
+        logger $CRITS_ADMIN $CRITS_ADMINPW
+
 
 fi
 
